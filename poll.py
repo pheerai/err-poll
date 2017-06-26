5918862983c5faef4b45fd7d2051b303d7d98843
Oliver Rümpelein <oli_r@fg4f.de>
2017-06-26 15:55:38 +0200

    Strip down functions and everything

--- poll.py
+++ poll.py
@@ -1,6 +1,7 @@
-from errbot import botcmd, BotPlugin
+from typing import Mapping, List
 
-from pollentry import PollEntry
+from errbot import botcmd, BotPlugin
+from errbot.backends.base import Identifier
 
 
 class Poll(BotPlugin):
@@ -147,9 +148,48 @@ class Poll(BotPlugin):
     #
     #         return '{}:\n{}'.format(current_poll, poll)
 
-    def reset_poll(self, title) -> None:
-        with self.mutable('polls') as polls:
-            poll = polls[title]
-            for option in poll.options:
-                poll.options[option] = 0
-            del poll.has_voted[:]
+    # def reset_poll(self, title) -> None:
+    #     with self.mutable('polls') as polls:
+    #         poll = polls[title]
+    #         for option in poll.options:
+    #             poll.options[option] = 0
+    #         del poll.has_voted[:]
+
+
+class PollEntry(object):
+    """
+    This is just a data object that can be pickled.
+    """
+    def __init__(self):
+        self._options = {}
+        self._has_voted = []
+
+    @property
+    def options(self) -> Mapping[str, int]:
+        return self._options
+
+    @property
+    def has_voted(self) -> List[Identifier]:
+        return self._has_voted
+
+    def __str__(self) -> str:
+        total_votes = sum(self._options.values())
+
+        result = ''
+        keys = sorted(self._options.keys())
+        for index, option in enumerate(keys):
+            votes = self._options[option]
+            result += '{} {}. {} ({} votes)\n'.format(drawbar(votes, total_votes), index+1, option, votes)
+
+        return result.strip()
+
+
+def drawbar(value, max_) -> str:
+    if max_:
+        value_in_chr = int(round((value * BAR_WIDTH / max_)))
+    else:
+        value_in_chr = 0
+    return '[' + '█' * value_in_chr + '▒' * int(round(BAR_WIDTH - value_in_chr)) + ']'
+
+
+BAR_WIDTH = 15.0
